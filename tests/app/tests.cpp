78593a8f9c310b5fb5016aba52895b22948e4eb4
Leo Benaducci <leobenaducci@hotmail.com>
2025-02-16 17:52:43 -0300

    added chaiscript enum

--- tests/app/tests.cpp
+++ tests/app/tests.cpp
@@ -3,15 +3,13 @@
 #include <utility>
 
 #include "chaiscript/chaiscript.hpp"
-#include "pybind11/pybind11.h"
+#include "chaiscript/utility/utility.hpp"
 
 #include "class.gen.h"
 #include "enum.gen.h"
 #include "pycppgen.gen.h"
 
-namespace py = pybind11;
-
-static void RegisterScriptingBindings(chaiscript::ChaiScript& chai, auto pyModule)
+static void RegisterScriptingBindings(chaiscript::ChaiScript& chai)
 {
     //chaiscript
     pycppgen_globals::for_each_type([&](auto param)
@@ -20,6 +18,27 @@ static void RegisterScriptingBindings(chaiscript::ChaiScript& chai, auto pyModul
 
             chai.add(chaiscript::user_type<T>(), pycppgen<T>::name());
             chai.add(chaiscript::constructor<T()>(), pycppgen<T>::name());
+        });
+
+    pycppgen_globals::for_each_enum([&](auto param)
+        {
+            using T = decltype(param);
+            const auto enumName = std::string(pycppgen<T>::name());
+
+            auto m = std::make_shared<chaiscript::Module>();
+    		chaiscript::utility::add_class(*m, enumName, std::vector<std::pair<T, std::string>>{});
+
+            pycppgen<T>::for_each_enum_value(
+                [&](auto v)
+                {
+                   m->add_global_const(chaiscript::const_var(T(v)), enumName + "__" + std::string(pycppgen<T>::enum_to_string(v)));
+                });
+
+            chai.add(chaiscript::type_conversion<T, std::underlying_type_t<T>>([](const T& v) { return static_cast<std::underlying_type_t<T>>(v); }));
+            chai.add(chaiscript::type_conversion<T, int>([](const T& v) { return static_cast<int>(v); }));
+            chai.add(chaiscript::type_conversion<T, std::string>([](const T& v) { return std::string(pycppgen<T>::enum_to_string(v)); }));
+
+            chai.add(m);
         });
 
     pycppgen_globals::for_each_type([&](auto param)
@@ -56,102 +75,23 @@ static void RegisterScriptingBindings(chaiscript::ChaiScript& chai, auto pyModul
         });
 }
 
-PYBIND11_EMBEDDED_MODULE(tests, m)
-{
-    //pocketpy
-	pycppgen_globals::for_each_type([&](auto param)
-		{
-			using T = decltype(param)::type;
-
-			bool parentRegistered = false;
-			pycppgen<T>::for_each_parent([&](auto parent)
-				{
-					if (parentRegistered)
-						return;
-
-					parentRegistered = true;
-
-					using parent_t = decltype(parent)::type;
-
-					auto& pyClass = py::class_<T, parent_t>(m, pycppgen<T>::name())
-						.def(py::init<>())
-                        .def(py::init<const T&>());
-
-					pycppgen<T>::for_each_var([&](auto var)
-						{
-							if (var.is_const())
-								pyClass.def_readonly(var.name(), var.VariablePtr);
-							else
-								pyClass.def_readwrite(var.name(), var.VariablePtr);
-						}, 0);
-
-					pycppgen<T>::for_each_function([&](auto fn)
-						{
-							pyClass.def(fn.name(), fn.FunctionPtr);
-						}, 0);
-				}, 1);
-
-			if (!parentRegistered)
-			{
-				auto& pyClass = py::class_<T>(m, pycppgen<T>::name())
-					.def(py::init<>());
-
-				pycppgen<T>::for_each_var([&](auto var)
-					{
-						if (var.is_const())
-							pyClass.def_readonly(var.name(), var.VariablePtr);
-						else
-							pyClass.def_readwrite(var.name(), var.VariablePtr);
-					}, 0);
-
-				pycppgen<T>::for_each_function([&](auto fn)
-					{
-						pyClass.def(fn.name(), fn.FunctionPtr);
-					}, 0);
-			}
-		});
-}
-
 int main()
 {
     chaiscript::ChaiScript chai;
-    py::scoped_interpreter guard{};
 
     SStructTest testStruct;
     testStruct.x = 75;
     testStruct.y = 0;
     testStruct.z = 100;
 
-    RegisterScriptingBindings(chai, py::module_::__main__());
-
-	{
-		printf("------------ PYTHON ---------------------\n");
-		auto globals = py::dict();
-
-		auto tests = py::module::import("tests");
-		globals["testStruct"] = py::cast(&testStruct);
-		py::exec(
-			"import tests\n"
-			"testStruct.z = 23\n"
-			"print('testStruct.z = ' + str(testStruct.z))\n"
-			"o2 = tests.CObject()\n"
-			"print('o2.PublicShort = ' + str(o2.PublicShort))\n"
-			"print('set o2.PublicShort to 2')\n"
-			"o2.PublicShort = 2\n"
-			, globals);
-
-		auto o2 = py::eval("o2", globals).cast<CObject>();
-		printf("o2.PublicShort = %d\n", o2.PublicShort);
-
-		printf("---------------------------------\n");
-	}
+    RegisterScriptingBindings(chai);
 
     try
     {
         printf("------------ CHAISCRIPT ---------------------\n");
 
         chai.add(chaiscript::var(testStruct), "testStruct");
-        auto o2 = chai.eval<CObject>(R"_(
+        auto o2 = chai.eval<CObject>(R"_chai_(
 		    print("SStructBase__size = " + to_string(SStructBase__size));
 		    print("testStruct.z = " + to_string(testStruct.z));
 		    print("testStruct.components = " + to_string(testStruct.components));
@@ -159,8 +99,9 @@ int main()
 		    print("o2.PublicShort = " + to_string(o2.PublicShort));
 		    print("set o2.PublicShort to 1");
             o2.PublicShort = 1;
+			print("EEnum__Test = " + to_string(EEnum__Test));
             return o2;
-	    )_");
+	    )_chai_");
 
         printf("o2.PublicShort = %d\n", o2.PublicShort);
     }
